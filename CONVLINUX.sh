526f1600f660d9ad091b886e003dcbc41208d2e6
Wiimpathy <39578591+Wiimpathy@users.noreply.github.com>
2021-01-21 19:33:48 +0100

    Update CONVLINUX.sh
    (it was the wrong file haha!)

--- CONVLINUX.sh
+++ CONVLINUX.sh
@@ -4,21 +4,40 @@ clear
 OIFS="$IFS"
 IFS=$'\n'
 
-EXE="./bin/createcat"
+FFMPEG="ffmpeg"
+AVTHP="./bin/avthp"
 
-#OPTION1=-debug
-#OPTION2=-fuzzy
-#OPTION3=-mobysearch
-#OPTION4=-skiphidden
+FPS=25
+BORDER=8
+WIDTH=320
+HEIGHT=240
+DURATION=30
+QUALITY=80
+NOAUDIO="--noaudio"
+#SKIP="--frameskip"
+
+mkdir THP
 
 if [[ -d $1 ]]; then
-	for file in `find "$1" -type f -name "*.db"`  
+	#for file in `find "$1" -type f -name "*.db"`
+    for file in `find "$1" -type f -name "*"`
 	do
-		echo
-		$EXE "$file" $OPTION1 $OPTION2 $OPTION3 $OPTION4
+	    THPFILE="${file##*/}"
+	    THPFILE="${THPFILE%.*}.thp"
+		echo "$file -> $OUTFILE"
+
+        $FFMPEG -y -i "$file" -filter:v "fps=fps=$FPS, drawbox= w=iw : h=ih : t=$BORDER : color=black" -q:v 0 -c:a copy "THP/tempz.mp4"
+        $AVTHP -s ${WIDTH}x${HEIGHT} -t $DURATION -q $QUALITY $NOAUDIO $SKIP "THP/tempz.mp4" "THP/$THPFILE"
+        rm "THP/tempz.mp4"
 	done
 elif [[ -f $1 ]]; then
-	$EXE "$1" $OPTION1 $OPTION2 $OPTION3 $OPTION4
+    THPFILE="${1##*/}"
+    THPFILE="${THPFILE%.*}.thp"
+    echo "$file -> $OUTFILE"
+
+    $FFMPEG -y -i "$1" -filter:v "fps=fps=$FPS, drawbox= w=iw : h=ih : t=$BORDER : color=black" -q:v 0 -c:a copy "THP/tempz.mp4"
+    $AVTHP -s ${WIDTH}x${HEIGHT} -t $DURATION -q $QUALITY $NOAUDIO $SKIP "THP/tempz.mp4" "THP/$THPFILE"
+    rm "THP/tempz.mp4"
 else
 	echo "$1 not a file or folder!"
 	exit 1
